4c79dbf1ee44598e2fd2eda24c3f6d831f4fb8ae
giovannivicentin <giovannifvicentin@gmail.com>
2024-08-09 17:56:38 -0300

    chore: Add internationalization support for homepage title

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,11 @@
+import { useTranslations } from "next-intl";
 
 export default function Home() {
+
+  const t = useTranslations('HomePage');
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     ola
+      App
     </main>
   );
 }
---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+
+export default function NotFound() {
+  return (
+    <html>
+        <body>
+            <h1>Something went wrong!</h1>
+        </body>
+    </html>
+    
+  )
+}
