54fc98d33df33809be083df53e243772ca9235cd
giovannivicentin <giovannifvicentin@gmail.com>
2024-08-10 00:28:44 -0300

    chore: Update not-found page with more descriptive error message

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <html>
         <body>
-            <h1>Something went wrong!</h1>
+            <h1>Error 404: Page not found.</h1>
         </body>
     </html>
     
