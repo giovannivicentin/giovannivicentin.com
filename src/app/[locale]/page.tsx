3c1298b1f295593ed14a7192936dd06af9a9e52e
giovannivicentin <giovannifvicentin@gmail.com>
2024-08-09 23:08:52 -0300

    feat: create a setup for internationalizate the portfolio website

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,3 +1,4 @@
+import LocalSwitcher from "@/components/local-switcher";
 import { useTranslations } from "next-intl";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-4xl font-bold">{t('title')}</h1>
+      <LocalSwitcher />
     </main>
   );
 }
