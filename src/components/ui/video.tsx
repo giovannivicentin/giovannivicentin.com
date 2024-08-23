5dfdea71590d5636087eb1d517afa3e1119e1088
giovannivicentin <giovannifvicentin@gmail.com>
2024-08-23 16:50:12 -0300

    feat: Add Video component for displaying videos with fallback image

---
+++ src/components/ui/video.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image'
+
+export function Video({ path, photo }: { path: string; photo: string }) {
+  return (
+    <video
+      width="1920"
+      height="1080"
+      controls
+      preload="none"
+      className="aspect-video w-full"
+    >
+      <source src={path} type="video/mp4" />
+      <Image
+        src={photo}
+        alt="Fallback image when video cannot be played"
+        width="640"
+        height="480"
+      />
+      <p>Your browser does not support the video tag.</p>
+    </video>
+  )
+}
