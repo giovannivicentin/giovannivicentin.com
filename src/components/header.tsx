e5dcc1083cbbc37917bb421f8b97d4ce0f565b5e
giovannivicentin <giovannifvicentin@gmail.com>
2024-08-26 16:43:45 -0300

    feat: Update header with logo and link to homepage

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,13 +1,22 @@
-import { Briefcase } from 'lucide-react'
+import Image from 'next/image'
 import Link from 'next/link'
 import { Navbar } from './navbar'
 
 export function Header() {
   return (
     <header className="fixed z-40 flex w-full items-center justify-between bg-background/90 px-4 py-3 backdrop-blur-md md:px-6 md:py-4">
-      <Link href="/" className="group flex items-center gap-2">
-        <Briefcase className="h-6 w-6 text-primary group-hover:text-muted-foreground" />
-        <span className="text-lg font-semibold group-hover:text-muted-foreground">
+      <Link
+        href="/"
+        className="flex items-center gap-2 rounded-md p-1 hover:bg-neutral-100 dark:hover:bg-transparent hover:dark:brightness-75"
+      >
+        <Image
+          src="/logos/white.svg"
+          alt="Logo of Giovanni Vicentin"
+          width={24}
+          height={24}
+          className="h-6 w-6 text-primary invert-0 dark:invert"
+        />
+        <span className="hidden text-lg font-semibold md:block">
           Giovanni Vicentin
         </span>
       </Link>
