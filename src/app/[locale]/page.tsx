8ac726ef9f6e7a21fab5712568675fc5ef8c520f
giovannivicentin <giovannifvicentin@gmail.com>
2024-08-27 11:32:42 -0300

    feat: create skill component to separate the section of skills in a block

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,7 @@
 import { ContactForm } from '@/components/contactForm'
 import Presentation from '@/components/presentation'
 import { Projects } from '@/components/projects'
-
+import { Skills } from '@/components/skills'
 import { Badge } from '@/components/ui/badge'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 import { BriefcaseIcon } from 'lucide-react'
@@ -46,58 +46,7 @@ export default function Home() {
         viewProjects={t('viewProjects')}
       />
       <Projects />
-      <section
-        id="skills"
-        className="flex w-full flex-col items-center justify-center space-y-8 bg-background px-4 py-12 md:px-12"
-      >
-        <h2
-          className={`${merriweather.variable} text-center font-serif text-3xl font-bold md:text-4xl`}
-        >
-          Skills & Technologies
-        </h2>
-        <div className="flex max-w-6xl flex-wrap items-center justify-center gap-2 px-2">
-          {[
-            'JavaScript',
-            'TypeScript',
-            'GraphQL',
-            'Node.js',
-            'React',
-            'Next.js',
-            'SQL',
-            'VTEX IO',
-            'React Native',
-            'Python',
-            'Jest',
-            'Vitest',
-            'Scrum',
-            'Git',
-            'Docker',
-            'Kubernetes',
-            'Bitbucket',
-            'Vite',
-            'Google Analytics',
-            'Axios',
-            'Sass',
-            'Tailwind',
-            'Zod',
-            'Styled-Components',
-            'Shadcn/ui',
-            'Radix UI',
-            'Postgres',
-            'MySQL',
-            'MongoDB',
-            'SQLite',
-          ].map((skill) => (
-            <Badge
-              key={skill}
-              variant="secondary"
-              className="px-3 py-1 text-lg"
-            >
-              {skill}
-            </Badge>
-          ))}
-        </div>
-      </section>
+      <Skills />
       <section
         id="experience"
         className="flex w-full flex-col space-y-8 bg-muted px-4 py-12 md:px-12"
---
+++ src/components/skills.tsx
@@ -0,0 +1,65 @@
+import { useTranslations } from 'next-intl'
+import { Merriweather } from 'next/font/google'
+import { Badge } from './ui/badge'
+
+const merriweather = Merriweather({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  display: 'swap',
+  variable: '--font-merriweather',
+})
+
+export function Skills() {
+  const t = useTranslations('skills')
+
+  return (
+    <section
+      id="skills"
+      className="flex w-full flex-col items-center justify-center space-y-8 bg-background px-4 py-12 md:px-12"
+    >
+      <h2
+        className={`${merriweather.variable} text-center font-serif text-3xl font-bold md:text-4xl`}
+      >
+        {t('title')}
+      </h2>
+      <div className="flex max-w-6xl flex-wrap items-center justify-center gap-2 px-2">
+        {[
+          'JavaScript',
+          'TypeScript',
+          'GraphQL',
+          'Node.js',
+          'React',
+          'Next.js',
+          'SQL',
+          'VTEX IO',
+          'React Native',
+          'Python',
+          'Jest',
+          'Vitest',
+          'Scrum',
+          'Git',
+          'Docker',
+          'Kubernetes',
+          'Bitbucket',
+          'Vite',
+          'Google Analytics',
+          'Axios',
+          'Sass',
+          'Tailwind',
+          'Zod',
+          'Styled-Components',
+          'Shadcn/ui',
+          'Radix UI',
+          'Postgres',
+          'MySQL',
+          'MongoDB',
+          'SQLite',
+        ].map((skill) => (
+          <Badge key={skill} variant="secondary" className="px-3 py-1 text-lg">
+            {skill}
+          </Badge>
+        ))}
+      </div>
+    </section>
+  )
+}
