c2d941c0f931bdee56470feadc10b7a3e5e211fa
giovannivicentin <giovannifvicentin@gmail.com>
2024-08-28 20:00:30 -0300

    refactor: Update font size and styling for Home page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   const t = useTranslations('Presentation')
 
   return (
-    <main className="flex flex-col items-center justify-between">
+    <main className="flex flex-col items-center justify-between font-sans text-base">
       <Presentation
         title={t('title')}
         description={t('description')}
