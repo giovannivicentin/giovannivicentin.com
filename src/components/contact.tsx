5579d17956b10a1da0574113bbcc59352e91a967
giovannivicentin <giovannifvicentin@gmail.com>
2024-08-28 09:50:48 -0300

    feat: Update contact form with translations and validation

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,3 +1,4 @@
+import { useTranslations } from 'next-intl'
 import { Merriweather } from 'next/font/google'
 import { ContactForm } from './contactForm'
 import { Card, CardContent, CardHeader, CardTitle } from './ui/card'
@@ -10,6 +11,9 @@ const merriweather = Merriweather({
 })
 
 export function Contact() {
+  // const t = useTranslations('ContactSection')
+  const f = useTranslations('ContactForm')
+
   return (
     <section id="contact" className="w-full bg-background px-4 py-12 md:px-12">
       <div className="grid gap-8 md:grid-cols-2">
@@ -23,7 +27,19 @@ export function Contact() {
             I&rsquo;d love to hear from you! Feel free to reach out with any
             questions or inquiries.
           </p>
-          <ContactForm />
+          <ContactForm
+            validationSubject={f('validationSubject')}
+            validationEmail={f('validationEmail')}
+            validationMessage={f('validationMessage')}
+            submitSuccess={f('submitSuccess')}
+            submitError={f('submitError')}
+            submitDescriptionError={f('submitDescriptionError')}
+            unknownError={f('unknownError')}
+            placeholderSubject={f('placeholderSubject')}
+            placeholderEmail={f('placeholderEmail')}
+            placeholderMessage={f('placeholderMessage')}
+            submitButton={f('submitButton')}
+          />
         </div>
         <div className="flex flex-col gap-4">
           <Card className="shadow-sm">
