6f640ed0adf28c77fbfbe93f6bd300df9ff05f49
giovannivicentin <giovannifvicentin@gmail.com>
2024-08-28 19:30:13 -0300

    refactor: Update NotFound component to use a larger font size and bold styling

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,9 @@ export default function NotFound() {
   return (
     <html>
       <body>
-        <h1>Error 404: Page not found.</h1>
+        <h1 className="font-serif text-4xl font-bold">
+          Error 404: Page not found.
+        </h1>
       </body>
     </html>
   )
