c2aac562f9a9d8ccecc8daec4b6f1d7dfaf65840
giovannivicentin <giovannifvicentin@gmail.com>
2024-08-30 18:01:55 -0300

    feat: deploy

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -16,6 +16,7 @@ export default function Home() {
         contactMe={t('contactMe')}
         viewProjects={t('viewProjects')}
       />
+
       <Projects />
       <Skills />
       <Experience />
