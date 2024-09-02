1a54d901e234bc4b49cf208f9f98ae797e22fbb1
giovannivicentin <giovannifvicentin@gmail.com>
2024-09-01 21:55:48 -0300

    feat: add presentation component to use next/image for better image loading and performance

--- src/components/presentation.tsx
+++ src/components/presentation.tsx
@@ -1,9 +1,8 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { Skeleton } from '@/components/ui/skeleton'
-import dynamic from 'next/dynamic'
 import { Merriweather } from 'next/font/google'
+import Image from 'next/image'
 import { useCallback } from 'react'
 
 const merriweather = Merriweather({
@@ -11,11 +10,6 @@ const merriweather = Merriweather({
   weight: ['400', '700'],
   display: 'swap',
   variable: '--font-merriweather',
-})
-
-const DynamicImage = dynamic(() => import('next/image'), {
-  loading: () => <Skeleton className="h-96 w-96 rounded-full" />,
-  ssr: false,
 })
 
 interface PresentationProps {
@@ -47,43 +41,40 @@ const Presentation: React.FC<PresentationProps> = ({
   }, [])
 
   return (
-    <section
-      id="presentation"
-      className="mb-32 mt-16 flex w-full flex-col items-center justify-between gap-8 px-4 md:mb-64 md:mt-24 md:flex-row md:gap-0 md:px-12"
-    >
-      <div className="space-y-4">
-        <h1
-          className={`${merriweather.variable} font-serif text-4xl font-bold md:text-5xl`}
-        >
-          {title}
-        </h1>
-        <p className="max-w-md font-medium md:max-w-xl md:text-lg">
-          {description}
-        </p>
-        <div className="flex items-center gap-4">
-          <Button
-            className="text-center text-base font-semibold"
-            onClick={() => handleSmoothScroll('#contact')}
+    <section id="presentation" className="min-h-screen w-full pt-6 md:pt-12">
+      <div className="flex w-full flex-col items-center justify-between gap-8 px-4 md:flex-row md:gap-0 md:px-12">
+        <div className="space-y-4">
+          <h1
+            className={`${merriweather.variable} font-serif text-4xl font-bold md:text-5xl`}
           >
-            {contactMe}
-          </Button>
-          <Button
-            className="text-center text-base font-semibold"
-            variant="outline"
-            onClick={() => handleSmoothScroll('#projects')}
-          >
-            {viewProjects}
-          </Button>
+            {title}
+          </h1>
+          <p className="max-w-md font-medium md:max-w-xl md:text-lg">
+            {description}
+          </p>
+          <div className="flex items-center gap-4">
+            <Button
+              className="text-center text-base font-semibold"
+              onClick={() => handleSmoothScroll('#contact')}
+            >
+              {contactMe}
+            </Button>
+            <Button
+              className="text-center text-base font-semibold"
+              variant="outline"
+              onClick={() => handleSmoothScroll('#projects')}
+            >
+              {viewProjects}
+            </Button>
+          </div>
         </div>
-      </div>
-      <div className="relative h-96 w-96">
-        <DynamicImage
+        <Image
           src="/images/profile/image.png"
           alt="@giovannivicentin"
           width={512}
           height={512}
           priority
-          className="rounded-full dark:grayscale md:h-96 md:w-96"
+          className="min-w-[12rem] rounded-full shadow-md dark:grayscale"
         />
       </div>
     </section>
