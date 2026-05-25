cbe2374ab6c3e2534ee907e2e893e48faf80e65b
Giovanni Vicentin <giovannifvicentin@gmail.com>
2026-05-25 20:02:10 -0300

    chore: add Prettier configuration and ignore files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,22 @@
 import rocketseatConfig from '@rocketseat/eslint-config/react.mjs'
+import eslintConfigPrettier from 'eslint-config-prettier'
 import nextConfig from 'eslint-config-next/core-web-vitals'
 
 const config = [
+  {
+    ignores: ['.next/**', 'coverage/**', 'next-env.d.ts', 'node_modules/**'],
+  },
   ...rocketseatConfig,
   ...nextConfig,
+  eslintConfigPrettier,
   {
     rules: {
       '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-require-imports': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
+      '@stylistic/indent': 'off',
+      '@stylistic/max-len': 'off',
+      '@stylistic/multiline-ternary': 'off',
       'no-undef': 'off',
       'react-hooks/set-state-in-effect': 'off',
     },
